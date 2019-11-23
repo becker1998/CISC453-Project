8c258ac7f7df4ac8789ffa8a321ee20d7f24ad8b
EternalDragonX <eternaldragonx@hotmail.com>
2019-11-23 11:00:20 -0500

    Add files via upload

---
+++ Board.py
@@ -0,0 +1,31 @@
+import random
+import numpy as np
+
+class Board():
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.board = [[0] * cols] * rows 
+
+    def isSlotFilled(self, row, col):
+        return self.board[row][column] != 0
+
+    def isColFilled(self, col):
+        return self.board[0][col] != 0
+
+    def insertPiece(self, player, col):
+        if not isColFilled(col):
+            for i in range(self.cols, -1, -1):
+                if self.board[i][cols] == 0:
+                    self.board[i][cols] = player
+                    break
+            # check if we win here
+
+    def isBoardFilled(self):
+        for j in range(self.cols):
+            if self.board[0][j] == 0:
+                return False
+        return True
+
+x = Board(6, 7)
+print (x.board)
