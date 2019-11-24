e6cff63c34ff574a869ec153a521f01f44148f3a
Tyler Beckstead <becker1998.tb@gmail.com>
2019-11-23 20:53:19 -0500

    Update QAgent.py

--- QAgent.py
+++ QAgent.py
@@ -38,16 +38,7 @@ class QAgent(Agent):
                     valOpp += 1
                 else:
                     break
-        if valCurrent == 3:
-            return 4
-        else if valOpp == 3 and valCurrent != 3:
-            return valOpp
-        else if valOpp == 2 or valCurrent == 2:
-            return 2
-        else if valOpp == 1 or valCurrent == 1:
-            return 1
-        else:
-            return 0
+        return returnValue(valCurrent, valOpp)
     def evalRowFunction (self, col, numCol, board):
         '''heruistic function that counts number of roww connections'''
         valCurrent = 0
@@ -120,6 +111,131 @@ class QAgent(Agent):
                     else:
                         break
             
+        return returnValue(valCurrent, valOpp)
+    def evalDiagFunction (self, col, numCol, board):
+        valCurrent = 0
+        valOpp = 0
+        column = board[col - 1]
+        top = column.index(self.pTag)
+        below = column.index(self.oTag)
+        if top < below and top != 0:
+            emptySpots = top - 1
+        else if below < top and below != 0:
+            emptySpots = below - 1
+
+        if col == 1:
+            try:
+                if board[col][emptySpots + 1] == self.pTag:
+                    valCurrent += 1
+                    for i in range (2,4):
+                        if board[i][emptySpots + 1] == self.pTag:
+                            valCurrent += 1
+                        else:
+                            break
+                else if board[col][emptySpots + 1] == self.oTag:
+                    valOpp += 1
+                    for i in range (2,4):
+                        if board[i][emptySpots + 1] == self.oTag:
+                            valOpp += 1
+                        else:
+                            break
+            except IndexError:
+                valCurrent = valCurrent
+                valOpp = valOpp
+            try:
+                if board[col][emptySpots - 1] == self.pTag:
+                    valCurrent += 1
+                    for i in range (2,4):
+                        if board[i][emptySpots - 1] == self.pTag:
+                            valCurrent += 1
+                        else:
+                            break
+                else if board[col][emptySpots - 1] == self.oTag:
+                    valOpp += 1
+                    for i in range (2,4):
+                        if board[i][emptySpots - 1] == self.oTag:
+                            valOpp += 1
+                        else:
+                            break
+            except IndexError:
+                valCurrent = valCurrent
+                valOpp = valOpp
+        else if col == numCol:
+            try:
+                if board[col - 1][emptySpots + 1] == self.pTag:
+                    valCurrent += 1
+                    for i in range (col-2,col-4):
+                        if board[i][emptySpots + 1] == self.pTag:
+                            valCurrent += 1
+                        else:
+                            break
+                else if board[col-1][emptySpots + 1] == self.oTag:
+                    valOpp += 1
+                    for i in range (col-2,col-4):
+                        if board[i][emptySpots + 1] == self.oTag:
+                            valOpp += 1
+                        else:
+                            break
+            except IndexError:
+                valCurrent = valCurrent
+                valOpp = valOpp
+            try:
+                if board[col-1][emptySpots - 1] == self.pTag:
+                    valCurrent += 1
+                    for i in range (col-2,col-4):
+                        if board[i][emptySpots - 1] == self.pTag:
+                            valCurrent += 1
+                        else:
+                            break
+                else if board[col-1][emptySpots - 1] == self.oTag:
+                    valOpp += 1
+                    for i in range (col-2,col-4):
+                        if board[i][emptySpots - 1] == self.oTag:
+                            valOpp += 1
+                        else:
+                            break
+            except IndexError:
+                valCurrent = valCurrent
+                valOpp = valOpp
+        else:
+            try:
+                if board[col - 1][emptySpots + 1] == self.pTag and board[col + 1][emptySpots - 1] != self.pTag:
+                    valCurrent += 1
+                    for i in range (col-2,col-4):
+                        if board[i][emptySpots + 1] == self.pTag:
+                            valCurrent += 1
+                        else:
+                            break
+                else if board[col - 1][emptySpots + 1] == self.oTag and board[col + 1][emptySpots - 1] != self.oTag:
+                    valOpp += 1
+                    for i in range (col-2,col-4):
+                        if board[i][emptySpots + 1] == self.oTag:
+                            valOpp += 1
+                        else:
+                            break
+                if board[col - 1][emptySpots + 1] == self.pTag and board[col + 1][emptySpots - 1] == self.pTag:
+                    valCurrent += 2
+                    if board[col - 2][emptySpots + 2] == self.pTag or board[col + 2][emptySpots - 2] == self.pTag:
+                        valCurrent +=1
+                if board[col + 1][emptySpots - 1] == self.pTag and board[col - 1][emptySpots + 1] == self.pTag:
+                    valCurrent += 2
+                    if board[col + 2][emptySpots - 2] == self.pTag or board[col - 2][emptySpots + 2] == self.pTag:
+                        valCurrent +=1
+                if board[col - 1][emptySpots + 1] == self.oTag and board[col + 1][emptySpots - 1] == self.oTag:
+                    valOpp += 2
+                    if board[col - 2][emptySpots + 2] == self.oTag or board[col + 2][emptySpots - 2] == self.oTag:
+                        valOpp +=1
+                if board[col + 1][emptySpots - 1] == self.oTag and board[col - 1][emptySpots + 1] == self.oTag:
+                    valOpp += 2
+                    if board[col + 2][emptySpots - 2] == self.oTag or board[col - 2][emptySpots + 2] == self.oTag:
+                        valOpp +=1 
+            except IndexError:
+                valCurrent = valCurrent
+                valOpp = valOpp
+                            
+        return returnValue(valCurrent, valOpp)
+    
+    def returnValue (self, valCurrent, valOpp):
         if valCurrent == 3:
             return 4
         else if valOpp == 3 and valCurrent != 3:
@@ -130,9 +246,6 @@ class QAgent(Agent):
             return 1
         else:
             return 0
-    def evalDiagFunction (self, col, numCol, board):
-        return 1
-
     def heuristicValue (self, col, numCol, board):
         return max([evalDiagFunction(col, numCol, board), evalRowFunction(col, numCol, board),
                     evalColFunction(col, board)])
