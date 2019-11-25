2d90b7fd6ac844ea89263bf839bde0a0826187f5
Tyler Beckstead <becker1998.tb@gmail.com>
2019-11-25 16:54:57 -0500

    n

--- Coin.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import random as rand
-
-class Coin:
-    def __init__(self):
-        
--- Human.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import random as rand
-import Agent
-
-class Human (Agent):
-    def __init__(self):
-        
