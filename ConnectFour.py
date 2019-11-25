ab63dcddd3b3e5d40fba00fc883945ce594c6811
EternalDragonX <eternaldragonx@hotmail.com>
2019-11-25 10:42:25 -0500

    Update ConnectFour.py

--- ConnectFour.py
+++ ConnectFour.py
@@ -25,16 +25,17 @@ class ConnectFour:
         player = board[lastPiece[0], lastPiece[1]]
         
         # check top and bot scores
-        while lastPiece[0] - 1 >= 0 and board[lastPiece[0] - 1][lastPiece[1] + 1] == player:
+        while lastPiece[0] - 1 >= 0 and board[lastPiece[0] - 1][lastPiece[1]] == player:
             topscore += 1
-        while lastPiece[0] + 1 <= 5 and board[lastPiece[0] - 1][ lastPiece[1] + 1] == player:
+        while lastPiece[0] + 1 <= 5 and board[lastPiece[0] - 1][ lastPiece[1]] == player:
             botscore += 1
         if topscore + botscore >= 4:
             return player
 
         # check topright and botleft scores
-        print ("check board: ", board)
-        print ("check lastPiece: ", lastPiece)
+        print ("check board:")
+        print(board)
+        print ("check lastPiece:", lastPiece)
         while lastPiece[0] - 1 >= 0 and lastPiece[1] + 1 <= 6 and board[lastPiece[0] - 1][ lastPiece[1] + 1] == player:
             toprightscore += 1
         while lastPiece[0] + 1 <= 5 and lastPiece[1] - 1 >= 0 and board[lastPiece[0] + 1][ lastPiece[1] - 1] == player:
@@ -61,6 +62,122 @@ class ConnectFour:
         # we didn't win
         return 0
 
+    def checkBoardState(self, player, rows, cols, board):
+        # array of all the connections of three for the given player
+        threeconnections = []
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] != 0:
+                    tempconnection = []
+                    topscore = 1
+                    botscore = 1
+                    toprightscore = 1
+                    botleftscore = 1
+                    rightscore = 1
+                    leftscore = 1
+                    botrightscore = 1
+                    topleftscore = 1
+                    
+                    # check top and bot scores
+                    while i - 1 >= 0 and board[i - 1, j] == player:
+                        topscore += 1
+                    while i + 1 <= 5 and board[i + 1, j] == player:
+                        botscore += 1
+                    if topscore + botscore == 3:
+                        tempconnection.append([i, j])
+                        for k in range(1, topscore):
+                            tempconnection.append([i - k, j])
+                        for k in range(1, botscore):
+                            tempconnection.append([i + k, j])
+                    tempconnection.sort()
+                    # check if top slot is free
+                    if tempconnection[0][0] - 1 >= 0 and board[tempconnection[0][0] - 1, tempconnection[0][1]] == 0:
+                        tempconnection.append([tempconnection[0][0] - 1, tempconnection[0][1]])
+                        tempconnection.sort()
+                        if tempconnection not in threeconnections:
+                            threeconnections.append(tempconnection)
+                    # check if bot slot is free
+                    if tempconnection[2][0] + 1 <= 5 and board[tempconnection[2][0] + 1, tempconnection[2][1]] == 0:
+                        tempconnection.append([tempconnection[2][0] + 1, tempconnection[2][1]])
+                        tempconnection.sort()
+                        if tempconnection not in threeconnections:
+                            threeconnections.append(tempconnection)
+                    
+                    # check topright and botleft scores
+                    while i - 1 >= 0 and j + 1 <= 6 and board[i - 1, j + 1] == player:
+                        toprightscore += 1
+                    while i + 1 <= 5 and j - 1 >= 0 and board[i + 1, j - 1] == player:
+                        botleftscore += 1
+                    if toprightscore + botleftscore == 3:
+                        tempconnection.append([i, j])
+                        for k in range(1, toprightscore):
+                            tempconnection.append([i - k, j + k])
+                        for k in range(1, botleftscore):
+                            tempconnection.append([i + k, j - k])
+                    tempconnection.sort()
+                    # check if topright slot is free
+                    if tempconnection[0][0] - 1 >= 0 and tempconnection[0][1] + 1 <= 6 and board[tempconnection[0][0] - 1, tempconnection[0][1] + 1] == 0:
+                        tempconnection.append([tempconnection[0][0] - 1, tempconnection[0][1] + 1])
+                        tempconnection.sort()
+                        if tempconnection not in threeconnections:
+                            threeconnections.append(tempconnection)
+                    # check if botleft slot is free
+                    if tempconnection[2][0] + 1 <= 5 and tempconnection[2][1] - 1 >= 0 and board[tempconnection[2][0] + 1, tempconnection[2][1] - 1] == 0:
+                        tempconnection.append([tempconnection[2][0] + 1, tempconnection[2][1] - 1])
+                        tempconnection.sort()
+                        if tempconnection not in threeconnections:
+                            threeconnections.append(tempconnection)
+
+                    # check right and left scores
+                    while j + 1 <= 6 and board[i, j + 1] == player:
+                        rightscore += 1
+                    while j - 1 >= 0 and board[i, j - 1] == player:
+                        leftscore += 1
+                    if rightscore + leftscore == 3:
+                        tempconnection.append([i, j])
+                        for k in range(1, rightscore):
+                            tempconnection.append([i, j + k])
+                        for k in range(1, leftscore):
+                            tempconnection.append([i, j - k])
+                    tempconnection.sort()
+                    # check if right slot is free
+                    if tempconnection[2][1] + 1 <= 6 and board[tempconnection[2][0], tempconnection[2][1] + 1] == 0:
+                        tempconnection.append([tempconnection[2][0], tempconnection[2][1] + 1])
+                        tempconnection.sort()
+                        if tempconnection not in threeconnections:
+                            threeconnections.append(tempconnection)
+                    # check if left slot is free 
+                    if tempconnection[0][1] - 1 >= 0 and board[tempconnection[0][0], tempconnection[0][1] - 1] == 0:
+                        tempconnection.append([tempconnection[0][0], tempconnection[0][1] - 1])
+                        tempconnection.sort()
+                        if tempconnection not in threeconnections:
+                            threeconnections.append(tempconnection)
+
+                    # check botright and topleft scores
+                    while i + 1 <= 5 and j + 1 <= 6 and board[i - 1, j] == player:
+                        botrightscore += 1
+                    while i - 1 >= 0 and j - 1 >= 0 and board[i + 1, j] == player:
+                        topleftscore += 1
+                    if botrightscore + topleftscore == 3:
+                        tempconnection.append([i, j])
+                        for k in range(1, botrightscore):
+                            tempconnection.append([i - k, j + k])
+                        for k in range(1, topleftscore):
+                            tempconnection.append([i + k, j - k])
+                    tempconnection.sort()
+                    # check if botright slot is free
+                    if tempconnection[2][0] + 1 <= 5 and tempconnection[2][1] + 1 <= 6 and board[tempconnection[2][0] + 1, tempconnection[2][1] + 1] == 0:
+                        tempconnection.append([tempconnection[2][0] + 1, tempconnection[2][1] + 1])
+                        tempconnection.sort()
+                        if tempconnection not in threeconnections:
+                            threeconnections.append(tempconnection)
+                    # check if topleft slot is free
+                    if tempconnection[0][0] - 1 >= 0 and tempconnection[0][1] - 1 >= 0 and board[tempconnection[0][0] - 1, tempconnection[0][1] - 1] == 0:
+                        tempconnection.append([tempconnection[0][0] - 1, tempconnection[0][1] - 1])
+                        tempconnection.sort()
+                        if tempconnection not in threeconnections:
+                            threeconnections.append(tempconnection)
+        return threeconnections
     def printGame (self,board):
         '''prints the game to the monitor'''
 
@@ -76,23 +193,24 @@ class ConnectFour:
         p2 = QAgent(2, self.actions, self.alpha, self.gamma, 1)
         
         insertP1 = p1.choice(board.getBoard(), board.isBoardEmpty())
-        print("insert : ", insertP1-1)
-        board.insertPiece(1,insertP1-1)
+        print("insert:", insertP1)
+        board.insertPiece(1, insertP1)
         insertP2 = p2.choice(board.getBoard(), board.isBoardEmpty())
-        board.insertPiece(2,insertP2-1)
+        board.insertPiece(2, insertP2)
         
         lastPiece = board.getLastPiece()
-        lastPiece[0] -= 1
-        lastPiece[1] -= 1
+        
         b = board.getBoard().T
-        print("Board 2: ", board.getBoard())
-        self.printGame(b)
+        print("Board 2:")
+        print(board.getBoard())
+        #self.printGame(b)
         b = board.getBoard().T
         #needs while loop to complete game
-        print(lastPiece)
         checkWin = self.checkForWin(board.getBoard(), lastPiece)
-        #while checkWin == 0:
-    
+        if checkWin == 0:
+            print ("no one won")
+        else:
+            print ("someone won")
 
-game = ConnectFour(6,7,1,1)
+game = ConnectFour(6, 7, 1, 1)
 game.play()
